5bec76a925e5fa501e654700af43c586b7aece73
Fábio Moura de Oliveira <fbsharp@hotmail.com>
2017-03-23 18:17:41 -0300

    Add project files.

---
+++ fbPokemon/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace fbPokemon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        // Na url abaixo, iremos retorna 100 pokemons por vez.
+        const string url = "http://pokeapi.co/api/v2/pokemon?limit=100";
+        
+
+        public async Task<List<Result>> ObterPokemons()
+        {
+            List<Result> resultado = null;
+
+            using (var httpClient = new System.Net.Http.HttpClient())
+            {
+                var jsonConteudo = await httpClient.GetStringAsync(url);
+
+                // Aqui, é a raiz do conteúdo json.
+                RootObject raizJson = JsonConvert.DeserializeObject<RootObject>(jsonConteudo);
+
+                // Zerar lista.
+                resultado?.Clear();
+                resultado = raizJson.results;
+
+
+
+                // Vamos obter todos os pokemons.
+                while (raizJson.next != null)
+                {
+                    // Vai para o proximo.
+                    jsonConteudo = await httpClient.GetStringAsync(raizJson.next);
+                    raizJson = JsonConvert.DeserializeObject<RootObject>(jsonConteudo);
+                    resultado.AddRange(raizJson.results);
+
+                    this.Text = raizJson.next;
+                }
+
+            }
+
+            return resultado;
+        }
+
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            List<Result> resultado = await ObterPokemons();
+
+
+            if(resultado != null)
+            {
+                foreach(var cadaResultado in resultado)
+                {
+                    listBox1.Items.Add(cadaResultado.name + ", " + cadaResultado.url);
+                }
+            }
+
+            MessageBox.Show("Executado com sucesso!!!", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
---
+++ fbPokemon/RootObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fbPokemon
+{
+    public class RootObject
+    {
+        public int count { get; set; }
+        public object previous { get; set; }
+        public List<Result> results { get; set; }
+        public string next { get; set; }
+    }
+}
